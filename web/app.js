ed124eac96c967ad908dd44ce33b4d3f0d6b6851
Tyler Funk <tcfunk25@gmail.fm>
2019-03-05 11:38:06 -0600

    show nodes/groups on frontend

--- web/app.js
+++ web/app.js
@@ -1,3 +1,23 @@
+Vue.config.devtools = true;
+
+var Node = Vue.component('Node', {
+  template: `
+    <span>{{ name }}</span>
+  `,
+  props: [
+    'name',
+  ]
+});
+
+var Group = Vue.component('Group', {
+  template: `
+    <span>Group {{ id }}</span>
+  `,
+  props: [
+    'id',
+  ]
+})
+
 var app = new Vue({
     el: '#app',
     data: function() {
@@ -5,24 +25,33 @@ var app = new Vue({
         isLoading: true,
         loadingMessage: "Loading...",
         currentTab: "nodes",
+        nodes: {},
+        groups: {},
       }
     },
     mounted: function() {
       var me = this;
       axios.get('http://localhost:8888/nodes.json').then(function(response) {
-        me.setData(response);
-        me.nodes = response;
-        if (this.isLoading) {
-          currentTab = "groups";
-          isLoading = false;
+        me.nodes = response.data;
+        if (me.isLoading) {
+          me.currentTab = "groups";
+          me.isLoading = false;
         }
       });
       axios.get('http://localhost:8888/groups.json').then(function(response) {
-        me.groups = response;
-        if (this.isLoading) {
-          currentTab = "nodes";
-          isLoading = false;
+        me.groups = response.data;
+        if (me.isLoading) {
+          me.currentTab = "nodes";
+          me.isLoading = false;
         }
       });
     },
+    methods: {
+      showNodes: function() {
+        this.currentTab = "nodes";
+      },
+      showGroups: function() {
+        this.currentTab = "groups";
+      }
+    }
   });
